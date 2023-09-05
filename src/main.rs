44f8589e3f9985f3d112f8f2e083fc5e4b34bda4
tyuiopzxc <2662248501@qq.com>
2023-09-05 23:02:57 +0800

    完成三个作业

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod buffer;
+mod compareString;
+mod iterator;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = buffer::Buffer::new();
+    buffer.push(1);
+    buffer.push(2);
+    buffer.push(3);
+
+    let sum = buffer.sum();
+    println!("Buffer 内元素和: {:?}", sum);
+
+    let x = "create";
+    let y = "rust";
+
+    let result = compareString::compareString(x, y);
+
+    if result {
+        println!("{} 字典序更大", x);
+    } else {
+        println!("{} 字典序更大", y);
+    }
+
+    let res: Vec<char> = iterator::iterator();
+    println!("{:?}", res);
 }
